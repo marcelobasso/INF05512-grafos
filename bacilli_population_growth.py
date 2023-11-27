fc8dee552fdf8470e0c9891ce823cdae40e8abba
Marcelo Basso <bassomarcelo.ba@gmail.com>
2023-11-27 13:24:23 -0300

    melhora comentarios e otimiza fibonacci

--- bacilli_population_growth.py
+++ bacilli_population_growth.py
@@ -1,5 +1,6 @@
 population_at_k = [0, 1]
 positions = []
+biggest_pos = 0
 
 # reads number of instances
 N_inst = int(input())
@@ -10,11 +11,14 @@ for i in range(N_inst):
     pos = int(n_inp[-12:len(n_inp)]) % 1500
     positions.append(pos)
 
-# calculate ellements till the biggest
-for x in range(2, 1500):
+    if pos > biggest_pos:
+        biggest_pos = pos
+
+# calculate ellements of fibonacci series till the biggest requested
+for x in range(2, biggest_pos + 1):
     pn = (population_at_k[x - 1] + population_at_k[x - 2]) % 1000
     population_at_k.append(pn)
 
-# print the number in order
+# print the requested numbers in order
 for i in range(N_inst):
     print(f"{population_at_k[positions[i]]:03d}")
