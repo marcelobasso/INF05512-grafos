bf09bc038909a48bab5ec3eb8574a06ed26babbe
Marcelo Basso <bassomarcelo.ba@gmail.com>
2023-12-02 09:18:22 -0300

    Obrigado jose

--- catalan_square.py
+++ catalan_square.py
@@ -6,7 +6,7 @@ c = [1 for i in range(n + 2)]
 for k in range(2, n + 2):
     # 2*k because its pairs of people
     # *2 because the sum mirrors
-    c[k] = c[k - 1] * (2 * k - 1) * 2 / (k + 1)
+    c[k] = c[k - 1] * (2 * k - 1) * 2 // (k + 1)
 
-    # calclculates the catalan number for n+1
-    print(int(c[n + 1]))
+# calclculates the catalan number for n+1
+print(c[n + 1])
